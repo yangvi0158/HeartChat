7b6b5388e4ec1bd781c58375c898ff12f2af75d7
VivianYang <viboloveyou12@gmail.com>
2023-08-24 20:06:39 +0800

    feat: init and style frontend side

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,116 @@
+import Image from 'next/image';
+import Stack from '@mui/material/Stack';
+import { motion, Variants } from "framer-motion";
+
+import { transition } from '@/app/animations/config';
+import '@/app/styles/index.sass';
+import styles from '@/app/styles/index.module.sass';
+import window1 from '../public/home-window1.svg';
+import window2 from '../public/home-window2.svg';
+import window3 from '../public/home-window3.svg';
+
+const MotionImage = motion(Image);
+const MotionStack = motion(Stack);
+
+const imageContainer: Variants = {
+    initial: {
+        transition: {
+            staggerChildren: 0.2,
+            staggerDirection: -1,
+        }
+    },
+    animate: {
+        transition: {
+            delay: .3,
+            staggerChildren: .3,
+            staggerDirection: 1,
+        },
+    },
+}
+
+const image: Variants = {
+    initial: {
+        y: 50,
+        opacity: 0,
+        transition: transition
+    },
+    animate: {
+        y: 0,
+        opacity: [0,80,100],
+        transition: transition
+    },
+}
+
+
+export default function HomePage() {
+    return (
+        <Stack direction="row" justifyContent="center" alignItems='center' className="container">
+            <Stack className="block">
+                <div className="header__title">
+                    <h1>
+                        <span className="title--pink">Heart</span>
+                        <span className="title--blue">Chat</span>
+                        <br></br>
+                        anytime,
+                        <br></br>
+                        anywhere
+                    </h1>
+                </div>
+                <p className="header__desc">
+                    HeartChat makes it easy and fun to quickly chat with people all around the globe.
+                </p>
+                <div>
+                    <button className={`${styles.button} button__signup`}>Sign In</button>
+                </div>
+            </Stack>
+            <MotionStack
+                className="block block__images"
+                alignItems='center'
+                initial="initial"
+                animate="animate"
+                exit="initial"
+                variants={imageContainer}
+            >
+                <MotionImage
+                    variants={image}
+                    priority
+                    alt="window-1"
+                    src={window1}
+                    style={{
+                        width: '100%',
+                        maxWidth: '400px',
+                        minWidth: '200px',
+                        height: 'auto',
+                        position: 'relative',
+                        top: '20px',
+                        left: '-20px'
+                    }}
+                ></MotionImage>
+                <MotionImage 
+                    variants={image}
+                    alt="window-2"
+                    src={window2}
+                    style={{
+                        width: '100%',
+                        maxWidth: '380px',
+                        height: 'auto',
+                        position: 'relative',
+                        right: '-70px'
+                    }}
+                ></MotionImage>
+                <MotionImage 
+                    variants={image}
+                    alt="window-3"
+                    src={window3}
+                    style={{
+                        width: '100%',
+                        maxWidth: '328px',
+                        height: 'auto',
+                        position: 'relative',
+                        top: '-15px'
+                    }}
+                ></MotionImage>
+            </MotionStack>
+        </Stack>
+    )
+}
---
+++ frontend/src/app/components/Room/RoomMain.tsx
@@ -0,0 +1,38 @@
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Tooltip from '@mui/material/Tooltip';
+import LogoutIcon from '@mui/icons-material/Logout';
+
+import '../../styles/room/roomMain.sass'
+import styles from '../../styles/index.module.sass';
+import roomStyles from '../../styles/room/room.module.sass';
+
+export default function RoomMain() {
+    return (
+        <div className={`${roomStyles.roomSection} roomMain`}>
+            <Stack
+                direction="row"
+                justifyContent="space-between"
+                alignItems="center"
+                className="roomMain--topBar"
+            >
+                <div className='chatroom-title'>
+                    <h2>Global Chatroom</h2>
+                </div>
+                <Stack direction="row">
+                    <Box sx={{mr: 2}}>
+                        <Tooltip title="Invite/Share">
+                            <button className={`${styles.button}`}>
+                                Invite
+                            </button>
+                        </Tooltip>
+                    </Box>
+                    <Tooltip title="Leave Room">
+                        <button className={`${styles.button} ${styles.buttonSecondary}`}><LogoutIcon/></button>
+                    </Tooltip>
+                </Stack>
+            </Stack>
+            <div className="chat--container"></div>
+        </div>
+    )
+}

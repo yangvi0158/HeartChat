a9b42e1fcea98228c9660afe3c1e77e6a26cd4ad
VivianYang <viboloveyou12@gmail.com>
2023-08-29 17:14:24 +0800

    feat: create useSnackBar hook

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app'
 import { Inter } from 'next/font/google'
 import { AnimatePresence } from "framer-motion";
 import { ThemeProvider } from '@mui/material/styles'
+import { SnackBarProvider } from '@/app/hooks/useSnackBar';
 import theme from '@/app/styles/theme';
 import '@/app/styles/global.sass'
 import '@/app/styles/normalize.css';
@@ -14,12 +15,14 @@ const inter = Inter({
 
 export default function App({Component, pageProps}: AppProps) {
     return (
-      <main className={inter.className}>
-        <ThemeProvider theme={theme}>
-          <AnimatePresence mode="wait">
-            <Component {...pageProps}/>
-          </AnimatePresence>
-        </ThemeProvider>
-      </main>
+        <main className={inter.className}>
+          <ThemeProvider theme={theme}>
+              <AnimatePresence mode="wait">
+                <SnackBarProvider>
+                  <Component {...pageProps}/>
+                </SnackBarProvider>
+              </AnimatePresence>
+          </ThemeProvider>
+        </main>
     )
 }

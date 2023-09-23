a2a0d8f5a7e80cbcd58357fa6376272e9d7be4a1
VivianYang <viboloveyou12@gmail.com>
2023-09-23 15:12:04 +0100

    feat: add usercontext and refactor

--- frontend/src/app/components/Chat/ChatBody.tsx
+++ frontend/src/app/components/Chat/ChatBody.tsx
@@ -1,44 +1,76 @@
+import { useRef, useEffect } from 'react';
 import Stack from '@mui/material/Stack';
+import dayjs from 'dayjs';
 
+import { useUser } from '../../contexts/UserContext';
+import { useRoom } from '../../contexts/RoomContext';
+import { useSocket } from '@/app/contexts/SocketContext';
 import '../../styles/chat/ChatBody.sass';
 
 export default function ChatBody() {
+    const { userData } = useUser();
+    const { messages } = useSocket();
+    const { currentRoom } = useRoom();
+    const { room_id } = currentRoom[0] || 0;
+    const chatBottomRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        chatBottomRef.current?.scrollIntoView({
+            behavior: "smooth"
+        });
+    }, [messages]);
+    
+
     return (
         <div className="chatBody">
-            <Stack className="message-item" direction="row">
-                <div className="avatar">GL</div>
-                <Stack>
-                    <p className="name">User1</p>
-                    <span className="message">
-                        Hello This is my first time using this app.
-                    </span>
-                    <p className="time">16:45 pm</p>
-                </Stack>
-            </Stack>
-            <Stack className="message-item" direction="row">
-                <div className="avatar">GL</div>
-                <Stack>
-                    <p className="name">User1</p>
-                    <span className="message">
-                        Hello This is my first time using this app.
-                        Hello This is my first time using this app.
-                        Hello This is my first time using this app.
-                        Hello This is my first time using this app.
-
-                    </span>
-                    <p className="time">16:45 pm</p>
-                </Stack>
-            </Stack>
-            <Stack className="message-item" direction="row">
-                <div className="avatar">GL</div>
-                <Stack>
-                    <p className="name">User1</p>
-                    <span className="message">
-                        Hello This is.
-                    </span>
-                    <p className="time">16:45 pm</p>
-                </Stack>
-            </Stack>
+            {messages[room_id] && (messages[room_id]).map((msg, key) => 
+                msg.socketId === 'wsSystem' ? (
+                    <Stack
+                        className="message-item"
+                        direction="row"
+                        justifyContent="center"
+                        key={key}
+                    >
+                        {msg.text}
+                    </Stack>
+                ) : (msg.id === userData.id) ? (
+                    <Stack
+                        className="message-item self"
+                        direction="row"
+                        justifyContent="end"
+                        key={key}
+                    >
+                        <Stack direction="column" alignItems="end" sx={{width: '100%'}}>
+                            <span className="message">{msg.text}</span>
+                            <p className="time">
+                                {dayjs(msg.time).format('HH:mm')}
+                            </p>
+                        </Stack>
+                    </Stack>
+                ) : (
+                    <Stack
+                        className="message-item"
+                        direction="row"
+                        key={key}
+                    >
+                        <div>
+                            <div className="avatar">
+                                {msg.name.slice(0,2)}
+                            </div>
+                        </div>
+                        <Stack alignItems="start" sx={{width: '100%'}}>
+                            <p className="name">{msg.name}</p>
+                            <span className="message">
+                                {msg.text}
+                            </span>
+                            <p className="time">
+                                {dayjs(msg.time).format('HH:mm')}
+                            </p>
+                        </Stack>
+                    </Stack>
+                )
+            )}
+            <div id="chatBody-bottom" ref={chatBottomRef}></div>
         </div>
     )
 }

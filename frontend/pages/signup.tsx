335472c987b353359c4983799764fa39949926fc
VivianYang <viboloveyou12@gmail.com>
2023-10-01 18:14:04 +0100

    fix: show loading page when user haven't fetch data in signup page

--- frontend/pages/signup.tsx
+++ frontend/pages/signup.tsx
@@ -8,15 +8,14 @@ import CircularProgress from '@mui/material/CircularProgress';
 
 import { colorList } from '@/app/configs/constant';
 import { socket } from '@/app/socket';
-import { useUser } from '@/app/contexts/UserContext'; 
-import '@/app/styles/signup.sass';
+import { useUser } from '@/app/contexts/UserContext';
 import styles from '@/app/styles/index.module.sass';
+import '@/app/styles/signup.sass';
 
 interface User {
     name: string;
     avatarColor: string;
 }
-
 
 export default function Signup() {    
     const { userData, isFetched } = useUser();
@@ -29,7 +28,7 @@ export default function Signup() {
 
     useEffect(() => {
         if (isFetched && userData.id) push('/room/init');
-    }, [userData.id])
+    }, [userData.id, isFetched])
 
     const addUser = () => {
         if (!userInfo.name.length) return;
@@ -45,73 +44,75 @@ export default function Signup() {
         push('/room/init');
     }
 
-    if (!isFetched) return (
-        <Stack justifyContent="center" alignItems="center" sx={{height: '100vh'}}>
-            <CircularProgress color="secondary" size="40px"/>
-        </Stack>
-    )
-
     return (
-        <Stack
-            direction="column"
-            justifyContent="center"
-            alignItems="center"
-            className="signup"
-        >
-            <Stack direction="row" alignItems="center">
-                {/* <Image src={Logo} alt="logo"></Image> */}
-                <p>Hello!</p>
+        <div>
+        { !isFetched || userData.id ? (
+            <Stack justifyContent="center" alignItems="center" sx={{height: '100vh'}}>
+                <CircularProgress color="secondary" size="40px"/>
             </Stack>
-            <span>
-                Choose a display name and pick a color that you like!
-            </span>
-            <TextField
-                id="outlined-basic"
-                label="Display Name"
-                variant="filled"
-                sx={{
-                    backgroundColor: '#3e4366',
-                    borderRadius: '10px',
-                    width: '300px',
-                    marginTop: '50px'
-                }}
-                onChange={(e) => setUserInfo((prev) => ({
-                    ...prev,
-                    name: e.target.value
-                }))}
-            />
+        ):(
             <Stack
-                direction="row"
-                alignItems="center"
+                direction="column"
                 justifyContent="center"
-                sx={{
-                    padding: '20px 0',
-                    margin: '20px 0'
-                }}
+                alignItems="center"
+                className="signup"
             >
-                {colorList.map((color, key) => (
-                    <Box
-                        key={key}
-                        className={`colorItem ${
-                            userInfo.avatarColor === color 
-                            && `selectedColor`}
-                        `}
-                        onClick={() => setUserInfo((prev) => ({
-                            ...prev,
-                            avatarColor: color
-                        }))}
-                        sx={{
-                            backgroundColor: color
-                        }}
-                    ></Box>
-                ))}
+                <Stack direction="row" alignItems="center">
+                    {/* <Image src={Logo} alt="logo"></Image> */}
+                    <p>Hello!</p>
+                </Stack>
+                <span>
+                    Choose a display name and pick a color that you like!
+                </span>
+                <TextField
+                    id="outlined-basic"
+                    label="Display Name"
+                    variant="filled"
+                    sx={{
+                        backgroundColor: '#3e4366',
+                        borderRadius: '10px',
+                        width: '300px',
+                        marginTop: '50px'
+                    }}
+                    onChange={(e) => setUserInfo((prev) => ({
+                        ...prev,
+                        name: e.target.value
+                    }))}
+                />
+                <Stack
+                    direction="row"
+                    alignItems="center"
+                    justifyContent="center"
+                    sx={{
+                        padding: '20px 0',
+                        margin: '20px 0'
+                    }}
+                >
+                    {colorList.map((color, key) => (
+                        <Box
+                            key={key}
+                            className={`colorItem ${
+                                userInfo.avatarColor === color 
+                                && `selectedColor`}
+                            `}
+                            onClick={() => setUserInfo((prev) => ({
+                                ...prev,
+                                avatarColor: color
+                            }))}
+                            sx={{
+                                backgroundColor: color
+                            }}
+                        ></Box>
+                    ))}
+                </Stack>
+                <button
+                    onClick={addUser}
+                    className={`${styles.button} button`}
+                >
+                    Let's Chat!
+                </button>
             </Stack>
-            <button
-                onClick={addUser}
-                className={`${styles.button} button`}
-            >
-                Let's Chat!
-            </button>
-        </Stack>
+        ) }
+        </div>
     )
 }
